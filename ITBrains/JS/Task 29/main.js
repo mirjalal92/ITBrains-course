afe0a5c544a7c10a0659143c6ed01bd3ee5c50cc
mirjalal92 <mirjalal.movsumov@mailru>
2024-02-23 15:22:40 +0400

    Task

---
+++ ITBrains/JS/Task 29/main.js
@@ -0,0 +1,62 @@
+// ======== Task 1 ======
+
+// let userObj = {
+//     name: "Mirjalal",
+//     birthdate: "14.07.1992",
+//     gender: "Male",
+//     age: 27,
+//     setAge: (year) => {
+//         return Product.age = year;
+//     }
+
+// }
+
+// console.log(userObj.birthdate);
+
+//  ===== Task 2=====
+
+// userObj.age = 27,
+
+//     console.log(userObj);
+
+// delete userObj.birthdate;
+
+// userObj.setAge(userObj.age)
+
+
+
+// ====== Task 3 ======
+
+// let userObj = {
+//     name: "Mirjalal",
+//     birthdate: "14.07.1992",
+//     gender: "Male",
+//     age: 27,
+//     setAge: function (secondAge) {
+//         return userObj.age = secondAge;
+//     },
+//     getYearBeforeRetirement: function () {
+//         return 65 - userObj.age;
+//     }
+// }
+
+
+// console.log(`İstifadəçinin dəyişən yaşı ${userObj.setAge(40)} `);
+// console.log(`Pensiya yashin hesablanmasi ${userObj.getYearBeforeRetirement()}`);
+
+
+// // ====== Task 4 =====
+
+let userObj = {
+
+    fullName: "Mirjalal",
+    creditLimit: "15000",
+    balans: "2500",
+    percentOfMinPayment: "5",
+
+    getBalance: function (realBalance) {
+        return userObj.balans = realBalance;
+    }
+}
+
+
